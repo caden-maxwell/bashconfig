92c1d23963be1f59a37e360f7107501d59b9ae73
cmax <a02335780@usu.edu>
2023-11-06 00:29:03 -0700

    agent issues

--- functions.sh
+++ functions.sh
@@ -35,6 +35,31 @@ function backup {
 }
 
 function gc() {
-    read -p "Enter your commit message: " message
-    git commit -m "$message"
+  read -p "Enter your commit message: " message
+  git commit -m "$message"
+}
+
+# Useful shortcuts for when I forget
+shortcuts() {
+cat <<-:
+Shortcut | Action
+---------|-----------------------------------------------
+  Up     | Bring up older commands from history
+  Down   | Bring up newer commands from history
+  Left   | Move cursor BACKWARD one character
+  Right  | Move cursor FORWARD one character
+Backspace| Erase the character to the LEFT of the cursor
+  Delete | Erase the character to the RIGHT the cursor
+  ^A     | Move cursor to BEGINNING of line
+  ^E     | Move cursor to END of line
+  M-B    | Move cursor BACKWARD one whole word
+  M-F    | Move cursor FORWARD one whole word
+  ^C     | Cancel (terminate) the currently running process
+  TAB    | Complete the command or filename at cursor
+  ^W     | Cut BACKWARD from cursor to beginning of word
+  ^K     | Cut FORWARD from cursor to end of line (kill)
+  ^Y     | Yank (paste) text to the RIGHT the cursor
+  ^L     | Clear the screen while preserving command line
+  ^U     | Cut the entire command line
+:
 }
--- general.sh
+++ general.sh
@@ -16,14 +16,6 @@ HISTFILESIZE=2000
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-# make less more friendly for non-text input files
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
@@ -36,20 +28,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
-# colored GCC warnings and errors
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
-fi
-
 # For the shortcut functions
 shopt -s cdable_vars
 dirsfile=~/.dirs
@@ -59,6 +37,5 @@ fi
 
 # nvm setup
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
